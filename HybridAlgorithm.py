3356e515e62f5aa52c71a172c8b26a2a79c8976a
keerthigan <keerthigan20@gmail.com>
2019-05-06 01:51:50 +0530

    hybrid and neural movie reco

---
+++ HybridAlgorithm.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 17:46:12 2019
+
+@author: keerthigan
+"""
+
+from surprise import AlgoBase
+
+class HybridAlgorithm(AlgoBase):
+
+    def __init__(self, algorithms, weights, sim_options={}):
+        AlgoBase.__init__(self)
+        self.algorithms = algorithms
+        self.weights = weights
+
+    def fit(self, trainset):
+        AlgoBase.fit(self, trainset)
+        
+        for algorithm in self.algorithms:
+            algorithm.fit(trainset)
+                
+        return self
+
+    def estimate(self, u, i):
+        
+        sumScores = 0
+        sumWeights = 0
+        
+        for idx in range(len(self.algorithms)):
+            sumScores += self.algorithms[idx].estimate(u, i) * self.weights[idx]
+            sumWeights += self.weights[idx]
+            
+        return sumScores / sumWeights
